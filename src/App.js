089fad025de9218af50184d8a18a38f0048e1e61
fAwkes1101 <abhishekyadav0751@gmail.com>
2022-08-14 00:41:25 +0530

    Die components rendered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react"
-import MainC from "./components/MainC.js"
+import Die from "./Die"
 import "./App.css"
-
-export default function App(){
+export default function App() {
     return (
-        
-        <>
-        what is thsi
-        <MainC/>
-        </>
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
     )
 }
