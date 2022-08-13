23006c1c6974e5bebb569e0878ec9b609569aa77
fAwkes1101 <abhishekyadav0751@gmail.com>
2022-08-14 01:12:24 +0530

    nanoId

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react"
-import Die from "./Die"
 import "./App.css"
 
-export default function App() {
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
 
+export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
     
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
@@ -19,7 +24,9 @@ export default function App() {
         setDice(allNewDice())
     }
     
-    const diceElements = dice.map(die => <Die value={die} />)
+    const diceElements = dice.map(die => (
+        <Die key={die.id} value={die.value} />
+    ))
     
     return (
         <main>
