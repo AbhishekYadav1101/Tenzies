7dc9f48b2639f402cf1f2c5d0667abbae023aa85
fAwkes1101 <abhishekyadav0751@gmail.com>
2022-08-14 00:16:18 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MainC from "./components/MainC.js"
+import "./App.css"
+
+export default function App(){
+    return (
+        
+        <>
+        what is thsi
+        <MainC/>
+        </>
+    )
+}
---
+++ src/components/MainC.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function MainC(){
+    return(
+        <div className="main">
+             I am in main
+        </div>
+    )
+}
